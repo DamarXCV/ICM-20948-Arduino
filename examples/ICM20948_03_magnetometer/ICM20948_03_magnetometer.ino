5f1d6c19e82ef4c30599036fe5299e56e6e8d4c1
Damar <nickwedermann@web.de>
2022-02-21 13:22:59 +0100

    Formated by WebKit

--- examples/ICM20948_03_magnetometer/ICM20948_03_magnetometer.ino
+++ examples/ICM20948_03_magnetometer/ICM20948_03_magnetometer.ino
@@ -2,10 +2,10 @@
   Example sketch for the ICM20948 library
 
   This sketch shows how to setup the magnetometer and how to query magnetometer
-  data. The magnetometer is a seperate unit on the module. It has its own I2C 
+  data. The magnetometer is a seperate unit on the module. It has its own I2C
   address. All read and write accesses are done via a kind of I2C "sub-bus". You
-  don't access the magnetometer registers directly. 
-   
+  don't access the magnetometer registers directly.
+
   Further information can be found on:
 
   https://wolles-elektronikkiste.de/icm-20948-9-achsensensor-teil-i (German)
@@ -13,8 +13,8 @@
 
 ***************************************************************************/
 
-#include <Wire.h>
 #include <ICM20948.h>
+#include <Wire.h>
 #define ICM20948_ADDR 0x68
 
 /* There are several ways to create your ICM20948 object:
@@ -25,47 +25,47 @@
  */
 ICM20948 myIMU = ICM20948(ICM20948_ADDR);
 
-void setup() {
-  Wire.begin();
-  Serial.begin(115200);
-  while (!Serial) {}
+void setup()
+{
+    Wire.begin();
+    Serial.begin(115200);
+    while (!Serial) { }
 
-  if (!myIMU.init()) {
-    Serial.println("ICM20948 does not respond");
-  }
-  else {
-    Serial.println("ICM20948 is connected");
-  }
+    if (!myIMU.init()) {
+        Serial.println("ICM20948 does not respond");
+    } else {
+        Serial.println("ICM20948 is connected");
+    }
 
-  if (!myIMU.initMagnetometer()) {
-    Serial.println("Magnetometer does not respond");
-  }
-  else {
-    Serial.println("Magnetometer is connected");
-  }
+    if (!myIMU.initMagnetometer()) {
+        Serial.println("Magnetometer does not respond");
+    } else {
+        Serial.println("Magnetometer is connected");
+    }
 
-  /* You can set the following modes for the magnetometer:
-   * AK09916_PWR_DOWN          Power down to save energy
-   * AK09916_TRIGGER_MODE      Measurements on request, a measurement is triggered by 
-   *                           calling setMagOpMode(AK09916_TRIGGER_MODE)
-   * AK09916_CONT_MODE_10HZ    Continuous measurements, 10 Hz rate
-   * AK09916_CONT_MODE_20HZ    Continuous measurements, 20 Hz rate
-   * AK09916_CONT_MODE_50HZ    Continuous measurements, 50 Hz rate
-   * AK09916_CONT_MODE_100HZ   Continuous measurements, 100 Hz rate (default)
-   */
-  myIMU.setMagOpMode(AK09916_CONT_MODE_20HZ);
+    /* You can set the following modes for the magnetometer:
+     * AK09916_PWR_DOWN          Power down to save energy
+     * AK09916_TRIGGER_MODE      Measurements on request, a measurement is triggered by
+     *                           calling setMagOpMode(AK09916_TRIGGER_MODE)
+     * AK09916_CONT_MODE_10HZ    Continuous measurements, 10 Hz rate
+     * AK09916_CONT_MODE_20HZ    Continuous measurements, 20 Hz rate
+     * AK09916_CONT_MODE_50HZ    Continuous measurements, 50 Hz rate
+     * AK09916_CONT_MODE_100HZ   Continuous measurements, 100 Hz rate (default)
+     */
+    myIMU.setMagOpMode(AK09916_CONT_MODE_20HZ);
 }
 
-void loop() {
-  myIMU.readSensor();
-  xyzFloat magValue = myIMU.getMagValues(); // returns magnetic flux density [µT]
+void loop()
+{
+    myIMU.readSensor();
+    xyzFloat magValue = myIMU.getMagValues(); // returns magnetic flux density [µT]
 
-  Serial.println("Magnetometer Data in µTesla: ");
-  Serial.print(magValue.x);
-  Serial.print("   ");
-  Serial.print(magValue.y);
-  Serial.print("   ");
-  Serial.println(magValue.z);
+    Serial.println("Magnetometer Data in µTesla: ");
+    Serial.print(magValue.x);
+    Serial.print("   ");
+    Serial.print(magValue.y);
+    Serial.print("   ");
+    Serial.println(magValue.z);
 
-  delay(1000);
+    delay(1000);
 }
--- examples/ICM20948_11_fsync_interrupt/ICM20948_11_fsync_interrupt.ino
+++ examples/ICM20948_11_fsync_interrupt/ICM20948_11_fsync_interrupt.ino
@@ -1,19 +1,19 @@
 /***************************************************************************
-* Example sketch for the ICM20948 library
-*
-* This sketch shows how to use the FSYNC interrupt. Since it can't propagate to the 
-* INT pin, you have to request the interrupt status. Please note that the FSYNC pin
-* has to be pulled up or down.
-*   
-* Further information can be found on:
-*
-* https://wolles-elektronikkiste.de/icm-20948-9-achsensensor-teil-i (German)
-* https://wolles-elektronikkiste.de/en/icm-20948-9-axis-sensor-part-i (English)
-* 
-***************************************************************************/
+ * Example sketch for the ICM20948 library
+ *
+ * This sketch shows how to use the FSYNC interrupt. Since it can't propagate to the
+ * INT pin, you have to request the interrupt status. Please note that the FSYNC pin
+ * has to be pulled up or down.
+ *
+ * Further information can be found on:
+ *
+ * https://wolles-elektronikkiste.de/icm-20948-9-achsensensor-teil-i (German)
+ * https://wolles-elektronikkiste.de/en/icm-20948-9-axis-sensor-part-i (English)
+ *
+ ***************************************************************************/
 
-#include <Wire.h>
 #include <ICM20948.h>
+#include <Wire.h>
 #define ICM20948_ADDR 0x68
 
 /* There are several ways to create your ICM20948 object:
@@ -27,57 +27,58 @@ ICM20948 myIMU = ICM20948(ICM20948_ADDR);
 const int intPin = 2;
 volatile bool fsyncEvent = false;
 
-void setup() {
-  Wire.begin();
-  Serial.begin(115200);
-  while(!Serial) {}
-  
-  if(!myIMU.init()){
-    Serial.println("ICM20948 does not respond");
-  }
-  else{
-    Serial.println("ICM20948 is connected");
-  }
- 
-  /* Set the interrupt pin:
-   * ICM20948_ACT_LOW  = active-low
-   * ICM20948_ACT_HIGH = active-high (default) 
-   */
-   //myIMU.setIntPinPolarity(ICM20948_ACT_LOW);
+void setup()
+{
+    Wire.begin();
+    Serial.begin(115200);
+    while (!Serial) { }
 
-  /*  If latch is enabled the interrupt pin level is held until the interrupt status 
-   *  is cleared. If latch is disabled the interrupt pulse is ~50µs (default).
-   */
-   myIMU.enableIntLatch(true);
+    if (!myIMU.init()) {
+        Serial.println("ICM20948 does not respond");
+    } else {
+        Serial.println("ICM20948 is connected");
+    }
 
-  /* The interrupts ICM20948_FSYNC_INT, ICM20948_WOM_INT and ICM20948_DMP_INT can be 
-   * cleared by any read or will only be cleared if the interrupt status register is 
-   * read (default).
-   */
-   //myIMU.enableClearIntByAnyRead(true);
+    /* Set the interrupt pin:
+     * ICM20948_ACT_LOW  = active-low
+     * ICM20948_ACT_HIGH = active-high (default)
+     */
+    // myIMU.setIntPinPolarity(ICM20948_ACT_LOW);
 
-  /* Set the FSync interrupt pin:
-   *  ICM20948_ACT_LOW  = interrupt if low
-   *  ICM20948_ACT_HIGH = interrupt if high (default) 
-   */
-   //myIMU.setFSyncIntPolarity(ICM20948_ACT_LOW);
+    /*  If latch is enabled the interrupt pin level is held until the interrupt status
+     *  is cleared. If latch is disabled the interrupt pulse is ~50µs (default).
+     */
+    myIMU.enableIntLatch(true);
 
-  /* The following interrupts can be enabled or disabled:
-   *  ICM20948_FSYNC_INT        FSYNC pin interrupt, can't propagate to the INT pin  
-   *  ICM20948_WOM_INT          Wake on motion
-   *  ICM20948_DATA_READY_INT   New data available
-   *  ICM20948_FIFO_OVF_INT     FIFO overflow
-   */
-  myIMU.enableInterrupt(ICM20948_FSYNC_INT);
-  //myIMU.disableInterrupt(ICM20948_DATA_READY_INT);
+    /* The interrupts ICM20948_FSYNC_INT, ICM20948_WOM_INT and ICM20948_DMP_INT can be
+     * cleared by any read or will only be cleared if the interrupt status register is
+     * read (default).
+     */
+    // myIMU.enableClearIntByAnyRead(true);
 
-  Serial.println("Pull down the FSYNC pin with a 10kOhm resistor.");
-  Serial.println("A HIGH signal to pin FSYNC will then cause an interrupt - try it!");
+    /* Set the FSync interrupt pin:
+     *  ICM20948_ACT_LOW  = interrupt if low
+     *  ICM20948_ACT_HIGH = interrupt if high (default)
+     */
+    // myIMU.setFSyncIntPolarity(ICM20948_ACT_LOW);
+
+    /* The following interrupts can be enabled or disabled:
+     *  ICM20948_FSYNC_INT        FSYNC pin interrupt, can't propagate to the INT pin
+     *  ICM20948_WOM_INT          Wake on motion
+     *  ICM20948_DATA_READY_INT   New data available
+     *  ICM20948_FIFO_OVF_INT     FIFO overflow
+     */
+    myIMU.enableInterrupt(ICM20948_FSYNC_INT);
+    // myIMU.disableInterrupt(ICM20948_DATA_READY_INT);
+
+    Serial.println("Pull down the FSYNC pin with a 10kOhm resistor.");
+    Serial.println("A HIGH signal to pin FSYNC will then cause an interrupt - try it!");
 }
 
-void loop() {
-  byte source = myIMU.readAndClearInterrupts();
-  if(myIMU.checkInterrupt(source, ICM20948_FSYNC_INT)){
-    Serial.println("Interrupt! Type: FSync Pin Interrupt");
-  }
+void loop()
+{
+    byte source = myIMU.readAndClearInterrupts();
+    if (myIMU.checkInterrupt(source, ICM20948_FSYNC_INT)) {
+        Serial.println("Interrupt! Type: FSync Pin Interrupt");
+    }
 }
